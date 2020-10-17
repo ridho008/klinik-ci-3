78b3a7f9864f03b4ed74c3acdd2113851769028f
ridho008 <ciuyy12@gmail.com>
2020-10-17 17:47:45 +0700

    crud pasien, obat, dokter, users

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,6 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller {
 	public function index()
 	{
+		$data['title'] = 'Dashboard';
+		$this->load->view('layout/header', $data);
+		$this->load->view('layout/sidebar');
 		$this->load->view('admin/dashboard');
+		$this->load->view('layout/footer');
 	}
 }
