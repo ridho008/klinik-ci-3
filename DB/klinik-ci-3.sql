44900adac324213c9c9213370f57e369f6a8ebdf
ridho008 <ciuyy12@gmail.com>
2020-10-19 22:33:23 +0700

    menambahkan DB sql

---
+++ DB/klinik-ci-3.sql
@@ -0,0 +1,261 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 19, 2020 at 05:29 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `klinik-ci-3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `berobat`
+--
+
+CREATE TABLE `berobat` (
+  `id_berobat` int(11) NOT NULL,
+  `id_pasien` int(11) NOT NULL,
+  `id_dokter` int(11) NOT NULL,
+  `tgl_berobat` date NOT NULL,
+  `keluhan` text NOT NULL,
+  `diagnosa` varchar(100) NOT NULL,
+  `penatalaksaan` enum('Rawat Jalan','Rawat Inap','Rujuk','Lainnya') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `berobat`
+--
+
+INSERT INTO `berobat` (`id_berobat`, `id_pasien`, `id_dokter`, `tgl_berobat`, `keluhan`, `diagnosa`, `penatalaksaan`) VALUES
+(1, 4, 4, '2020-10-17', 'Jatuh', 'Obat Merah', 'Rawat Inap'),
+(3, 1, 3, '2020-10-21', 'Meriang', 'Begadang', 'Rujuk');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dokter`
+--
+
+CREATE TABLE `dokter` (
+  `id_dokter` int(11) NOT NULL,
+  `nama_dokter` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dokter`
+--
+
+INSERT INTO `dokter` (`id_dokter`, `nama_dokter`) VALUES
+(1, 'Gani'),
+(3, 'Harun'),
+(4, 'Kakuki'),
+(5, 'asdasd'),
+(6, 'wr');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+
+CREATE TABLE `obat` (
+  `id_obat` int(11) NOT NULL,
+  `nama_obat` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `obat`
+--
+
+INSERT INTO `obat` (`id_obat`, `nama_obat`) VALUES
+(1, 'Paradox'),
+(2, 'Minipol'),
+(4, 'we');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pasien`
+--
+
+CREATE TABLE `pasien` (
+  `id_pasien` int(11) NOT NULL,
+  `nama_pasien` varchar(50) NOT NULL,
+  `jk_pasien` enum('L','P') NOT NULL,
+  `umur_pasien` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`id_pasien`, `nama_pasien`, `jk_pasien`, `umur_pasien`) VALUES
+(1, 'Manisam', 'P', 20),
+(2, 'Nisa', 'P', 21),
+(3, 'Hasan', 'P', 22),
+(4, 'Waniro', 'P', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resep_obat`
+--
+
+CREATE TABLE `resep_obat` (
+  `id_resep` int(11) NOT NULL,
+  `id_berobat` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `resep_obat`
+--
+
+INSERT INTO `resep_obat` (`id_resep`, `id_berobat`, `id_obat`) VALUES
+(1, 3, 1),
+(2, 3, 2),
+(3, 3, 2),
+(4, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `nama_lengkap` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `username`, `password`, `nama_lengkap`) VALUES
+(3, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Ridho Surya'),
+(5, 'asd', 'f10e2821bbbea527ea02200352313bc059445190', 'asda');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `berobat`
+--
+ALTER TABLE `berobat`
+  ADD PRIMARY KEY (`id_berobat`),
+  ADD KEY `id_dokter` (`id_dokter`),
+  ADD KEY `id_pasien` (`id_pasien`);
+
+--
+-- Indexes for table `dokter`
+--
+ALTER TABLE `dokter`
+  ADD PRIMARY KEY (`id_dokter`);
+
+--
+-- Indexes for table `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id_obat`);
+
+--
+-- Indexes for table `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id_pasien`);
+
+--
+-- Indexes for table `resep_obat`
+--
+ALTER TABLE `resep_obat`
+  ADD PRIMARY KEY (`id_resep`),
+  ADD KEY `id_obat` (`id_obat`),
+  ADD KEY `id_berobat` (`id_berobat`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `berobat`
+--
+ALTER TABLE `berobat`
+  MODIFY `id_berobat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `dokter`
+--
+ALTER TABLE `dokter`
+  MODIFY `id_dokter` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `resep_obat`
+--
+ALTER TABLE `resep_obat`
+  MODIFY `id_resep` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `berobat`
+--
+ALTER TABLE `berobat`
+  ADD CONSTRAINT `berobat_ibfk_1` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id_dokter`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `berobat_ibfk_2` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id_pasien`);
+
+--
+-- Constraints for table `resep_obat`
+--
+ALTER TABLE `resep_obat`
+  ADD CONSTRAINT `resep_obat_ibfk_1` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`) ON DELETE CASCADE,
+  ADD CONSTRAINT `resep_obat_ibfk_2` FOREIGN KEY (`id_berobat`) REFERENCES `berobat` (`id_berobat`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
